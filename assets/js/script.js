37f4b153077a9fc8335e0b4fb1db3cc8ed3bf4e4
Matheus Pompeo <matheuspompeo13@gmail.com>
2023-09-19 09:46:14 -0300

    tentativas de algumas coisas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let header = document.querySelector('header');
 
-window.addEventListener("scroll", function() {  //esse código faz com q mude o fundo do header quando scrolla a página (ChatGPT)
+window.addEventListener("scroll", function() {
     if (window.scrollY > 1) {
         header.classList.add("scrolled");
     } else {
