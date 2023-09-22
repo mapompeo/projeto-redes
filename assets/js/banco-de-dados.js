9200e20ea555cf65d6d2b602c3c0a82a2386609f
José Gabriel Santos Gomes <70996909+naasdd@users.noreply.github.com>
2023-09-22 11:35:51 -0300

    Criação página Banco-de-Dados

---
+++ assets/js/banco-de-dados.js
@@ -0,0 +1,9 @@
+let header = document.querySelector('header');
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 1) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
