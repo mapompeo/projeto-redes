38ed9b73e3cde9876cd4c3985fa23e62ce22cd6d
Kauã Biasebetti <kauamengel17@gmail.com>
2023-10-16 16:35:51 -0300

    Algoritmos arrumados
    - Descrição dos algoritmos em portugol elaborada
    
    - Criação do botão de download
    
    - Download linkado com o arquivo

--- assets/js/exercicio-03.js
+++ assets/js/exercicio-03.js
@@ -1,56 +1,56 @@
 let resValores = document.getElementById('resValores')
 let resOrdenada = document.getElementById('resOrdenada')
 let resMedia = document.getElementById('resMedia')
-let array = []
+let num_array = []
 
 function gerar03() {
     let random = 0
-    array = []
+    num_array = []
     
     for(i=0; i < 10; i++){
         random = Math.floor(Math.random() * (50 - 1 + 1) + 1)
-        array.push(random)
+        num_array.push(random)
     }
-    resValores.innerHTML = `Valores aleatórios: ${array.join(", ")}.`
+    resValores.innerHTML = `Valores aleatórios: ${num_array.join(", ")}.`
 }
 
 function cresc03( ){
 
-    for(i = 0; i < array.length; i++){
-        let atual = array[i]
+    for(i = 0; i < num_array.length; i++){
+        let atual = num_array[i]
         let menor = atual
         let posicao = i
     
-        for(j = i + 1; j < array.length; j++){
-            if(array[j] < menor){
-                menor = array[j]
+        for(j = i + 1; j < num_array.length; j++){
+            if(num_array[j] < menor){
+                menor = num_array[j]
                 posicao = j
             }
         }
-        array[i] = menor
-        array[posicao] = atual
+        num_array[i] = menor
+        num_array[posicao] = atual
     }   
-    resOrdenada.innerHTML = `Os valores em ordem crescente são = [${array}]`
+    resOrdenada.innerHTML = `Os valores em ordem crescente são = [${num_array}]`
 }
 
 function decre03(){
-    for(i = 0; i < array.length; i++){
-        let atual = array[i]
+    for(i = 0; i < num_array.length; i++){
+        let atual = num_array[i]
         let menor = atual
         let posicao = i
     
-        for(j = i + 1; j < array.length; j++){
-            if(array[j] < menor){
-                menor = array[j]
+        for(j = i + 1; j < num_array.length; j++){
+            if(num_array[j] < menor){
+                menor = num_array[j]
                 posicao = j
             }
         }
 
-        array[i] = menor
-        array[posicao] = atual
+        num_array[i] = menor
+        num_array[posicao] = atual
         
     }
-    resOrdenada.innerHTML = `Os valores em ordem decrescente são = [${array.reverse()}]`
+    resOrdenada.innerHTML = `Os valores em ordem decrescente são = [${num_array.reverse()}]`
 }
 
 function media03(){
@@ -59,13 +59,13 @@ function media03(){
     let mediaPar = 0
     let mediaImpar = 0
 
-    for(i=0; i <array.length; i++){
-        if(array[i] % 2 ==0){
+    for(i=0; i <num_array.length; i++){
+        if(num_array[i] % 2 ==0){
             par++
-            mediaPar += array[i]
+            mediaPar += num_array[i]
         }else{
             impar++
-            mediaImpar += array[i]
+            mediaImpar += num_array[i]
         }
     }
 
--- assets/js/ipi.js
+++ assets/js/ipi.js
@@ -18,6 +18,40 @@ function calcular(){
 
 
 function copiar() {
-  const textoACopiar = "codigo IPI";
-  navigator.clipboard.writeText(textoACopiar);
+  const codigoPortugol = `programa
+  {
+    
+    funcao inicio()
+    {
+      real ipi, p1_cod, p1_valor, p1_qtdn, p2_cod, p2_valor, p2_qtdn 
+  
+      escreva("~~~~~~~~~~~~~~~~~~~~\n")
+      escreva("Cálculo de IPI\n")
+      escreva("~~~~~~~~~~~~~~~~~~~~\n")
+      escreva("Insira o valor de IPI: ")
+      leia(ipi)
+      escreva("Insira o código da primeira peça: ")
+      leia(p1_cod)
+      escreva("Insira o valor da primeira peça: ")
+      leia(p1_valor)
+      escreva("Insira a quantidade necessária da primeira peça: ")
+      leia(p1_qtdn)
+  
+      escreva("Insira o código da segunda peça: ")
+      leia(p2_cod)
+      escreva("Insira o valor da segunda peça: ")
+      leia(p2_valor)
+      escreva("Insira a quantidade necessária da segunda peça: ")
+      leia(p2_qtdn)
+  
+      real resposta = (p1_valor * p1_qtdn + p2_valor * p2_qtdn) * (ipi / 100 + 1)
+      escreva("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+      escreva("O valor de IPI a ser pago é = ",resposta,"\n")
+      escreva("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+      
+  
+    }
+  }`
+  navigator.clipboard.writeText(codigoPortugol)
+  btcopiar.innerHTML = 'Copiado!'
 }
