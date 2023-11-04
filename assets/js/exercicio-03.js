99c2217c16e10302c24266b3d5ae7e8c40ef14a5
Kauã Biasebetti <kauamengel17@gmail.com>
2023-11-04 03:56:33 -0300

    Exercícios arrumados | Troca de imagem para ícone
    - Exercício 03 de JS refeito e arrumado
    
    - Troca dos elementos visuais na home (imagens para ícones)
    
    - Botão "Saiba Mais" na home arrumado (Empurrando texto pra cima)

--- assets/js/exercicio-03.js
+++ assets/js/exercicio-03.js
@@ -1,56 +1,56 @@
 let resValores = document.getElementById('resValores')
 let resOrdenada = document.getElementById('resOrdenada')
 let resMedia = document.getElementById('resMedia')
-let num_array = []
+let num_array_03 = []
 
 function gerar03() {
     let random = 0
-    num_array = []
+    num_array_03 = []
     
     for(i=0; i < 10; i++){
         random = Math.floor(Math.random() * (50 - 1 + 1) + 1)
-        num_array.push(random)
+        num_array_03.push(random)
     }
-    resValores.innerHTML = `Valores aleatórios: ${num_array.join(", ")}.`
+    resValores.innerHTML = `Valores aleatórios: ${num_array_03.join(", ")}.`
 }
 
-function cresc03( ){
+function cresc03(){
 
-    for(i = 0; i < num_array.length; i++){
-        let atual = num_array[i]
+    for(i = 0; i < num_array_03.length; i++){
+        let atual = num_array_03[i]
         let menor = atual
         let posicao = i
     
-        for(j = i + 1; j < num_array.length; j++){
-            if(num_array[j] < menor){
-                menor = num_array[j]
+        for(j = i + 1; j < num_array_03.length; j++){
+            if(num_array_03[j] < menor){
+                menor = num_array_03[j]
                 posicao = j
             }
         }
-        num_array[i] = menor
-        num_array[posicao] = atual
+        num_array_03[i] = menor
+        num_array_03[posicao] = atual
     }   
-    resOrdenada.innerHTML = `Os valores em ordem crescente são = [${num_array}]`
+    resOrdenada.innerHTML = `Os valores em ordem crescente são = ${num_array_03.join(", ")}.`
 }
 
 function decre03(){
-    for(i = 0; i < num_array.length; i++){
-        let atual = num_array[i]
+    for(i = 0; i < num_array_03.length; i++){
+        let atual = num_array_03[i]
         let menor = atual
         let posicao = i
     
-        for(j = i + 1; j < num_array.length; j++){
-            if(num_array[j] < menor){
-                menor = num_array[j]
+        for(j = i + 1; j < num_array_03.length; j++){
+            if(num_array_03[j] < menor){
+                menor = num_array_03[j]
                 posicao = j
             }
         }
 
-        num_array[i] = menor
-        num_array[posicao] = atual
+        num_array_03[i] = menor
+        num_array_03[posicao] = atual
         
     }
-    resOrdenada.innerHTML = `Os valores em ordem decrescente são = [${num_array.reverse()}]`
+    resOrdenada.innerHTML = `Os valores em ordem decrescente são = ${num_array_03.reverse().join(", ")}.`
 }
 
 function media03(){
@@ -59,13 +59,13 @@ function media03(){
     let mediaPar = 0
     let mediaImpar = 0
 
-    for(i=0; i <num_array.length; i++){
-        if(num_array[i] % 2 ==0){
+    for(i=0; i <num_array_03.length; i++){
+        if(num_array_03[i] % 2 ==0){
             par++
-            mediaPar += num_array[i]
+            mediaPar += num_array_03[i]
         }else{
             impar++
-            mediaImpar += num_array[i]
+            mediaImpar += num_array_03[i]
         }
     }
 
