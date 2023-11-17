5edd52c03e4cbcb4765377606f4d134b42d49a5b
Matheus <matheuspompeo13@gmail.com>
2023-11-17 14:57:55 -0300

    terminando de estilizar o carrossel

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,25 +1,34 @@
+//O carrossel não foi criado por nós, mas sim pelo ChatGPT. Ainda não possuimos o conhecimento suficiente para fazer este carrossel, mas a estilização foi totalmente realizada pelos devs
+
+console.log("O carrossel não foi criado por nós, mas sim pelo ChatGPT. Ainda não possuimos o conhecimento suficiente para fazer este carrossel, mas a estilização foi totalmente realizada pelos devs")
+
 let currentIndex = 0;
 
 function showSlide(index) {
-  const carouselContent = document.querySelector('.carousel-content');
-  const slides = document.querySelectorAll('.carousel-item');
-  const totalSlides = slides.length;
+    const carouselContent = document.querySelector('.carousel-content');
+    const slides = document.querySelectorAll('.carousel-item');
+    const paginationDots = document.querySelectorAll('.pagination-dot');
+    const totalSlides = slides.length;
 
-  if (index < 0) {
-    index = totalSlides - 1;
-  } else if (index >= totalSlides) {
-    index = 0;
-  }
+    if (index < 0) {
+        index = totalSlides - 1;
+    } else if (index >= totalSlides) {
+        index = 0;
+    }
 
-  const translateValue = -index * 100 + '%';
-  carouselContent.style.transform = 'translateX(' + translateValue + ')';
-  currentIndex = index;
+    const translateValue = -index * 100 + '%';
+    carouselContent.style.transform = 'translateX(' + translateValue + ')';
+    currentIndex = index;
+
+    // Atualizar indicadores de paginação
+    paginationDots.forEach(dot => dot.classList.remove('active'));
+    paginationDots[index].classList.add('active');
 }
 
 function prevSlide() {
-  showSlide(currentIndex - 1);
+    showSlide(currentIndex - 1);
 }
 
 function nextSlide() {
-  showSlide(currentIndex + 1);
+    showSlide(currentIndex + 1);
 }
