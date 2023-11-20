18c89adf1a50cb547891972f0b430695f1ec78ab
José Gabriel Santos Gomes <70996909+naasdd@users.noreply.github.com>
2023-11-20 12:22:38 -0300

    mais responsividade
    Co-Authored-By: Kauã Biasebetti <kauamengel17@gmail.com>
    Co-Authored-By: Matheus Pompeo <matheuspompeo13@gmail.com>

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,3 +10,25 @@ function show(){
         mostrarSenha.classList.replace('bi-eye-slash', 'bi-eye')
     }
 }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var boxLogin = document.querySelector(".box-login");
+//     var highlight = document.createElement("div");
+//     highlight.className = "highlight";
+//     document.body.appendChild(highlight);
+
+//     boxLogin.addEventListener("mousemove", function(event) {
+//         // Atualiza a posição e tamanho do destaque com base nas coordenadas do mouse
+//         var rect = boxLogin.getBoundingClientRect();
+//         highlight.style.width = event.clientX - rect.left + "px";
+//         highlight.style.height = event.clientY - rect.top + "px";
+//         highlight.style.left = rect.left + "px";
+//         highlight.style.top = rect.top + "px";
+//     });
+
+//     boxLogin.addEventListener("mouseleave", function() {
+//         // Remove o destaque quando o mouse sai do box-login
+//         highlight.style.width = "0";
+//         highlight.style.height = "0";
+//     });
+// });
